ce77fc6e8c961fe963013c760f9ed24cc444bf30
Sydney Goldberg <sydney.goldberg@outlook.com>
2020-07-20 18:52:59 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -6,7 +6,7 @@ end
 
 def count_empty_strings(array)
   return array.count do |element|
-  element == nil
+  element == ""
   end
   # Return the total number of EMPTY strings in the provided array using the count enumerable
 end
