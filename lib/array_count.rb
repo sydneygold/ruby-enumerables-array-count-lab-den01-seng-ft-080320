f3b1114dead365f18badfabc137721f5b6568a56
Sydney Goldberg <sydney.goldberg@outlook.com>
2020-07-20 18:47:05 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,7 +1,5 @@
 def count_strings(array)
-  return array.any? do |word| word == ""
-  # Return the total number of strings in the provided array using the count enumerable
-  end
+  return array.count do |""|
 end
 
 def count_empty_strings(array)
